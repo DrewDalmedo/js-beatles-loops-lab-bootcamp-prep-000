aef4252d66ecb367f4c7daec53ec8ece2dd07701
Andrew Dalmedo <andrewdalmedo@gmail.com>
2020-02-04 05:47:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
   // creating an empty variable
-  var empty = [];
+  var statements = [];
   // for loop (iterating through musicians)
   for(var i = 0; i < musicians.length; i++) {
-
+    statements.push(`${} plays ${}`);
   }
 
 }
