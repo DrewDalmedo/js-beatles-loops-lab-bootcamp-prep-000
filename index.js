417038e0b1dff93ade17d04e67018a72b44e858a
Andrew Dalmedo <andrewdalmedo@gmail.com>
2020-02-04 05:44:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
-  var empty = [];
+  var empty = new array();
   empty.push('test');
   console.log(empty);
 }
+
+theBeatlesPlay(new array(), new array());
