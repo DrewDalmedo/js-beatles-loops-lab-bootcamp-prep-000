6279565899bef2c2a9db3d6674e12436d99a6828
Andrew Dalmedo <andrewdalmedo@gmail.com>
2020-02-04 05:46:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ function theBeatlesPlay(musicians, instruments) {
   // creating an empty variable
   var empty = [];
   // for loop
-  
+  for(var i = 0; i < musicians.length; i++) {
+    
+  }
 
 }
 
